ae1dd77feb4c58d0032819d77d23855969bb8bdb
Etienne Jacquot <etiennej@upenn.edu>
2026-07-21 18:44:14 -0400

    code: deployment script for nextjs react build to S3 and Cloudfront

---
+++ deploy-static.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+# Etienne Jacquot
+# 06/22/2026
+
+export NAMED_PROFILE=...
+export SITE_NAME=...
+export CLOUDFRONT_DISTRIBUTION_ID=...
+
+echo "Building static site for $SITE_NAME using profile $NAMED_PROFILE"
+# Build static site
+cd app && npx next build
+
+echo "Syncing static site to S3 bucket s3://$SITE_NAME/"
+# Sync everything first
+aws s3 sync out/ s3://$SITE_NAME/ --delete --profile=$NAMED_PROFILE
+
+# Fix CSS content-type explicitly
+aws s3 sync out/_next/ s3://$SITE_NAME/_next/ \
+  --exclude "*" --include "*.css" \
+  --content-type "text/css" \
+  --profile=$NAMED_PROFILE
+
+# Fix JS content-type explicitly  
+aws s3 sync out/_next/ s3://$SITE_NAME/_next/ \
+  --exclude "*" --include "*.js" \
+  --content-type "application/javascript" \
+  --profile=$NAMED_PROFILE
+
+echo "Invalidating CloudFront cache for distribution $CLOUDFRONT_DISTRIBUTION_ID"
+# Invalidate CloudFront cache
+aws cloudfront create-invalidation \
+  --distribution-id $CLOUDFRONT_DISTRIBUTION_ID \
+  --paths "/*" \
+  --profile=$NAMED_PROFILE
+
+echo ""
+echo "Deployment complete for $SITE_NAME"
